775a35e0831053475762756b4a75410d9acb3309
ElonaKuqiiii <elona.kuqi@student.uni-pr.edu>
2025-11-11 19:02:56 +0100

    Implemented /list command

--- UDP_SERVER/shared.cpp
+++ UDP_SERVER/shared.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 #include "shared.h"
+#include <filesystem>
+#include <sstream>
 
-int main() {
-    std::cout << "Server starting...\n";
-    return 0;
+bool isAuthenticated = false;
+const char* DATA_DIR = "data";
+
+
+std::string listFiles();
+std::string readFile(const std::string& name);
+std::string deleteFile(const std::string& name);
+std::string searchFiles(const std::string& key);
+std::string fileInfo(const std::string& name);
+std::string uploadFile(const std::string& name, const std::string& content);
+std::string downloadFile(const std::string& name);
+
+std::string handleCommand(const std::string& cmd);
+
+
+
+std::string listFiles() {
+    std::stringstream ss;
+
+    for (const auto& entry : std::filesystem::directory_iterator(DATA_DIR)) {
+        ss << entry.path().filename().string() << "\n";
+    }
+    return ss.str();
 }
