e911c55333fa01a4d4b1601fc7041b8c3ee24961
ElsaKrasniqii <elsa.krasniqi12@student.uni-pr.edu>
2025-11-12 17:00:44 +0100

    shtimi i struktures ClientInfo

--- UDP_PROJECT_13/shared.h
+++ UDP_PROJECT_13/shared.h
@@ -10,3 +10,12 @@ extern const int TIMEOUT_SECONDS;
 #define BUFFER_SIZE 1024
 
 extern bool isAuthenticated;
+
+struct ClientInfo {
+    std::string key, ip;
+    int port = 0;
+    bool isAdmin = false;
+    int messagesReceived = 0;
+    int totalBytes = 0;
+    std::chrono::steady_clock::time_point lastActive;
+};
