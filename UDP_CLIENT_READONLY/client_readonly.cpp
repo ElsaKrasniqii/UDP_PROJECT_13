c3fa02896238e4489661af871c3d8e553e0af036
ElsaKrasniqii <elsa.krasniqi12@student.uni-pr.edu>
2025-11-12 17:37:04 +0100

    shtimi i ip se serverit tek client_readonly

---
+++ UDP_CLIENT_READONLY/client_readonly.cpp
@@ -0,0 +1,13 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS #define _CRT_SECURE_NO_WARNINGS
+
+#include <iostream>
+#include <string>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+
+#include "shared.h"
+#pragma comment(lib, "ws2_32.lib")
+
+using namespace std;
+
+#define SERVER_IP "127.0.0.1"  
