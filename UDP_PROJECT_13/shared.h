d6c6f416a3cd9fdc1d275ae9012270ceb3760ec5
ElsaKrasniqii <elsa.krasniqi12@student.uni-pr.edu>
2025-11-12 14:45:52 +0100

    shtimi i variablave dhe konstanteve globale

---
+++ UDP_PROJECT_13/shared.h
@@ -0,0 +1,12 @@
+﻿#pragma once #include <string>
+#include <chrono>
+
+extern const std::string ADMIN_SECRET;
+extern const char* DATA_DIR;
+
+extern const int PORT;
+extern const int TIMEOUT_SECONDS;
+
+#define BUFFER_SIZE 1024
+
+extern bool isAuthenticated;
